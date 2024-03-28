efc9f6d1c795cd63660906baa9c1e9d7edc975c5
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-03-28 20:06:39 +0900

    TypeScript Setting and Typing The Props and Optional Props

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+	return (
+		<div>
+			<Circle borderColor="blue" bgColor="tomato" />
+			<Circle text="안녕" bgColor="teal" />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Circle.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+	bgColor: string;
+	borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+	width: 200px;
+	height: 200px;
+	background-color: ${(props) => props.bgColor};
+	border-radius: 50%;
+	border: 3px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+	bgColor: string;
+	borderColor?: string;
+	text?: string;
+}
+
+function Circle({ bgColor, borderColor, text = "Default Text" }: CircleProps) {
+	return (
+		<Container bgColor={bgColor} borderColor={borderColor ?? "red"}>
+			{text}
+		</Container>
+	);
+}
+
+export default Circle;
