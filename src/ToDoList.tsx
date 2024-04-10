c6d41660f2b4ecb8e927b0ca31f32b3542209493
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-04-10 22:03:55 +0900

    use hook in react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-	const { register, watch } = useForm();
-	console.log(watch());
+	const { register, handleSubmit, formState } = useForm();
+	console.log(formState.errors);
+
+	const onValid = (data: any) => {
+		console.log(data);
+	};
 	return (
 		<div>
-			<form>
-				<input {...register("userName")} placeholder="userName" />
-				<input {...register("email")} placeholder="email" />
-				<input {...register("password")} placeholder="password" />
-				<input {...register("checkPassword")} placeholder="checkPassword" />
+			<form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
+				<input
+					{...register("userName", {
+						required: { value: true, message: "userName이 필요합니다!" },
+					})}
+					placeholder="userName"
+				/>
+				<input {...register("email", { required: true })} placeholder="email" />
+				<input {...register("password", { required: true })} placeholder="password" />
+				<input {...register("checkPassword", { required: true })} placeholder="checkPassword" />
 				<button>추가</button>
 			</form>
 		</div>
