d9e6d8829e8faeed5aaf7f08c0eddc5147c5f70d
황현민 <hwanghyunmin0650@gmail.com>
2024-04-17 07:38:54 +0900

    Update src/components/CreateToDo.tsx
    Co-authored-by: 강승훈 <102217780+HUN1i@users.noreply.github.com>

--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -46,7 +46,7 @@ function CreateToDo() {
 	const category = useRecoilValue(categoryState);
 	const { register, handleSubmit, setValue } = useForm<IForm>();
 	const onValid = ({ toDo }: IForm) => {
-		setToDos((oldToDos) => [{ text: toDo, id: Date.now(), category: category }, ...oldToDos]);
+setToDos((oldToDos) => [{ text: toDo, id: Date.now(), category }, ...oldToDos]);
 		setValue("toDo", "");
 	};
 	return (
