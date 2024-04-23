8bfc61c1f5921ed8bbdd2385c6235d681a19814e
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-04-23 12:04:04 +0900

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
-import { styled } from "styled-components";
+import styled from "styled-components";
 import { toDoState } from "./atoms";
 import Board from "./Components/Board";
 
@@ -18,22 +18,39 @@ const Boards = styled.div`
 	justify-content: center;
 	align-items: flex-start;
 	width: 100%;
-	gap: 15px;
+	gap: 10px;
 `;
 
 function App() {
 	const [toDos, setToDos] = useRecoilState(toDoState);
-
 	const onDragEnd = (info: DropResult) => {
-		const { draggableId, destination, source } = info;
+		const { destination, draggableId, source } = info;
+		if (!destination) return;
 		if (destination?.droppableId === source.droppableId) {
+			// same board movement.
 			setToDos((allBoards) => {
 				const boardCopy = [...allBoards[source.droppableId]];
+				const taskObj = boardCopy[source.index];
 				boardCopy.splice(source.index, 1);
-				boardCopy.splice(destination.index, 0, draggableId);
+				boardCopy.splice(destination?.index, 0, taskObj);
 				return {
 					...allBoards,
 					[source.droppableId]: boardCopy,
+				};
+			});
+		}
+		if (destination.droppableId !== source.droppableId) {
+			// cross board movement
+			setToDos((allBoards) => {
+				const sourceBoard = [...allBoards[source.droppableId]];
+				const taskObj = sourceBoard[source.index];
+				const destinationBoard = [...allBoards[destination.droppableId]];
+				sourceBoard.splice(source.index, 1);
+				destinationBoard.splice(destination?.index, 0, taskObj);
+				return {
+					...allBoards,
+					[source.droppableId]: sourceBoard,
+					[destination.droppableId]: destinationBoard,
 				};
 			});
 		}
--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -3,28 +3,35 @@ import { Draggable } from "react-beautiful-dnd";
 import { styled } from "styled-components";
 
 interface IDraggableCardProps {
-	toDo: string;
+	toDoId: number;
+	toDoText: string;
 	index: number;
 }
 
-function DraggableCard({ toDo, index }: IDraggableCardProps) {
-	console.log(toDo, "렌더");
+function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
+	// console.log(toDo, "렌더");
 	return (
-		<Draggable key={toDo} draggableId={toDo} index={index}>
-			{(provided) => (
-				<Card ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-					{toDo}
+		<Draggable key={toDoId} draggableId={toDoId + ""} index={index}>
+			{(provided, snapshot) => (
+				<Card
+					isDragging={snapshot.isDragging}
+					ref={provided.innerRef}
+					{...provided.dragHandleProps}
+					{...provided.draggableProps}
+				>
+					{toDoText}
 				</Card>
 			)}
 		</Draggable>
 	);
 }
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
 	border-radius: 5px;
 	padding: 10px 10px;
 	margin-bottom: 5px;
-	background-color: ${(props) => props.theme.cardColor};
+	background-color: ${(props) => (props.isDragging ? "#74b9ff" : props.theme.cardColor)};
+	box-shadow: ${(props) => (props.isDragging ? "0px 2px 2px rgba(0, 0, 0, 0.1)" : "none")};
 `;
 
 export default React.memo(DraggableCard);
