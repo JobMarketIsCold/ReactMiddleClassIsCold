e62f038d8320b4aace7d4321b6353bb96d966de5
hyunmin200 <hwanghyunmin0650@gmail.com>
2024-04-24 17:56:27 +0900

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,47 +11,32 @@ const Wrapper = styled(motion.div)`
 	height: 100vh;
 	width: 100vw;
 	display: flex;
-	justify-content: center;
+	justify-content: space-around;
 	align-items: center;
-	flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
 	width: 400px;
-	height: 200px;
+	height: 400px;
 	background-color: rgba(255, 255, 255, 1);
-	border-radius: 40px;
-	position: absolute;
-	top: 50px;
+	border-radius: 15px;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-	entry: (back: boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }),
-	center: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.3 } },
-	exit: (back: boolean) => ({
-		x: back ? 500 : -500,
-		opacity: 0,
-		scale: 0,
-		transition: { duration: 0.3 },
-	}),
-};
+const Circle = styled(motion.div)`
+	background-color: #00a5ff;
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-	const [visible, setVisible] = useState(1);
-	const [back, setBack] = useState(false);
-	const prevPlease = async () => {
-		await setBack(true);
-		setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-	};
-
-	const nextPlease = async () => {
-		await setBack(false);
-		setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-	};
+	const [clicked, setClicked] = useState(false);
+	const toggleClicked = () => setClicked((prev) => !prev);
 	const x = useMotionValue(0);
 	const gradient = useTransform(
 		x,
@@ -62,21 +47,11 @@ function App() {
 		],
 	);
 	return (
-		<Wrapper style={{ background: gradient }}>
-			<AnimatePresence custom={back}>
-				<Box
-					custom={back}
-					variants={box}
-					initial="entry"
-					animate="center"
-					exit="exit"
-					key={visible}
-				>
-					{visible}
-				</Box>
-			</AnimatePresence>
-			<button onClick={nextPlease}>Next</button>
-			<button onClick={prevPlease}>Prev</button>
+		<Wrapper onClick={toggleClicked} style={{ background: gradient }}>
+			<Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
+			<Box>
+				{clicked ? <Circle layoutId="circle" style={{ scale: 2 }} /> : null}
+			</Box>
 		</Wrapper>
 	);
 }
